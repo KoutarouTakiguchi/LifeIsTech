d8a227ea555e7d10fccb27aff64a2d2bdd013509
滝口光太郎 <>
2018-05-05 00:04:20 +0900

    実装項目 ・TrailRendererによる尾の調整 ・ゴールオブジェクトの設置 ・エネルギーバーと障害物の追加

---
+++ Assets/script/player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour {
+	public float enegy = 10;
+	Slider slider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<Rigidbody> ().velocity = transform.right * 30f;
+
+
+//エネルギーゲージ
+		slider = GameObject.Find ("Slider").GetComponent<Slider> ();
+		slider.value = enegy;
+		
+	}
+	void OnCollisionEnter(Collider other){
+		if (other.gameObject.name == "enemy") {
+			enegy--;
+		}
+	}
+}
