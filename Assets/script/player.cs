14913ebd88b7e6464e2ad509ed416de73f816914
滝口光太郎 <>
2018-05-10 20:12:03 +0900

    5/10最新 ・バウンド＿完了

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -4,27 +4,48 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class player : MonoBehaviour {
-	public float enegy = 10;
-	Slider slider;
+	public float enegy = 0.0f;
+	public Rigidbody rb;
+	public GameObject rect;
+	public GameObject pl;
+	float angle;
+	Vector3 rotee;
+
+	public Slider slider;
+
+
+    [SerializeField]public Transform  target;
+	[SerializeField]public Transform  cursor;
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody> ();
+		rb.AddForce (10, 0, 10, ForceMode.VelocityChange);
+
+		slider.maxValue = enegy;
+		slider.value = enegy;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		gameObject.GetComponent<Rigidbody> ().velocity = transform.right * 30f;
-
-
+		Vector3 targetDir = rotee - transform.position;
+	    angle = Vector3.Angle (targetDir, transform.forward);
+		transform.LookAt (rotee);
 //エネルギーゲージ
-		slider = GameObject.Find ("Slider").GetComponent<Slider> ();
-		slider.value = enegy;
+//		slider = GameObject.Find ("Slider").GetComponent<Slider> ();
 		
 	}
-	void OnCollisionEnter(Collider other){
+	void OnCollisionEnter(Collision other){
 		if (other.gameObject.name == "enemy") {
 			enegy--;
+			slider.value = enegy;
+		}else if(other.gameObject.tag == "wall") {
+			rotee = pl.transform.position;
+
+
 		}
 	}
+//	void rote(){
+//		rotee = transform.position;
+//	}
 }
